849a9a4307340400aeabb66a730d9ac527fb034e
TeuTeuf <antoine.teuf@gmail.com>
2020-04-04 22:10:03 +0200

    use sequelize Model

--- src/app/infra/SqlMarketRepository.ts
+++ src/app/infra/SqlMarketRepository.ts
@@ -1,6 +1,6 @@
 import * as MarketRepository from '../domain/MarketRepository'
 import { Market } from '../domain/Market'
-import { DataTypes } from 'sequelize'
+import { DataTypes, Model } from 'sequelize'
 import { sequelize } from './Sequelize'
 
 sequelize.define('market', {
@@ -17,18 +17,42 @@ sequelize.define('market', {
   updatedAt: false
 })
 
+class SqlMarket extends Model<SqlMarket> {
+  id!: string
+  name!: string
+}
+
+SqlMarket.init({
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  createdAt: false,
+  updatedAt: false,
+  tableName: 'market'
+})
+
 export const saveMarket: MarketRepository.SaveMarket = async (market) => {
   try {
-    await sequelize.model('market').create(market)
+    await SqlMarket.create(market)
   } catch (e) {
     console.error(e)
   }
 }
 
 export const findMarket: MarketRepository.FindMarket = async (id) => {
-  const sqlMarket = await sequelize.model('market').findByPk(id)
-  return {
-    id: sqlMarket?.get('id'),
-    name: sqlMarket?.get('name')
-  } as Market
+  const sqlMarket = await SqlMarket.findByPk(id)
+  return sqlMarket
+    ? {
+      id: sqlMarket.id as string,
+      name: sqlMarket.name as string
+    }
+    : undefined
 }
